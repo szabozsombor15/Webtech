8a7701c6f43ba5be5fc1f7125529ab155caecff2
szabozsombor15 <zsombor.atos.szabo@student.uni-miskolc.hu>
2026-03-24 10:53:09 +0100

    7. gyak

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
-function searchUser() {
+function searchRepo() {
     const input = document.getElementById("searchInput").value;
     const resultsDiv = document.getElementById("results");
 
@@ -37,4 +37,13 @@ function searchUser() {
             console.error("Hiba:", error);
             alert("Hiba történt!");
         });
+}
+
+function loadNavbar() {
+    fetch("navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("navbar-container").innerHTML = data;
+        })
+        .catch(error => console.error("Navbar hiba:", error));
 }
