c3d26168de2c1f8a5327a609a8453ec532b1ad17
szabozsombor15 <zsombor.atos.szabo@student.uni-miskolc.hu>
2026-04-22 18:48:38 +0200

    Feladatok rendszerezve

---
+++ 6-7.Gyak (egymásra épülnek)/script2.js
@@ -0,0 +1,13 @@
+function loadNavigation() {
+    fetch('./navbar.html')
+        .then(res => res.text())
+        .then(navbarHtml => {
+            document.body.insertAdjacentHTML('afterbegin', navbarHtml);
+        })
+        .catch(err => {
+            console.error(err);
+            alert('Hiba a menürendszer betöltésekor.');
+        });
+}
+
+loadNavigation();
